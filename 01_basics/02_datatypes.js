8a266fd630483224a3eea1cf1fe6782ede5e9435
Sahil-athlete <134266260+Sahil-athlete@users.noreply.github.com>
2023-05-23 07:22:19 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict" //treat all js code as newer version
+
+//alert(3+3) //we are using node js not browser
+
+console.log(3+
+    3) // code readablitity should be high 
+
+//datatypes 
+//number =>  2 to power of 53
+//string =>  ""  
+//null => standalone value 
+//undefiened =>doent assign the value till now
+//boolean => true/false 
+//question for the interview null is object or not? 
+//yes null is also a object.    
+//symbol => unique
+
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
