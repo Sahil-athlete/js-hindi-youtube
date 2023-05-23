fcdf40fec2d4c0a449efb82fbc6e3f4d6b5b1ceb
Sahil-athlete <134266260+Sahil-athlete@users.noreply.github.com>
2023-05-23 06:14:23 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 12233
+let accountEmail = "asssa@google.com"
+var accountPassword = 123455
+accountCity = "jaipur"
+// accountid = 2 //not allowed
+
+accountEmail = "dsaji@google.com"
+accountPassword = 1232122
+accountCity = "mumbai"
+let accountState;
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+/*
+prefer to not use var 
+because the block scope and functional s/c
+*/
