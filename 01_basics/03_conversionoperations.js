c9de8f12927cc867275bf068ee0e2c558eaf7b21
Sahil-athlete <134266260+Sahil-athlete@users.noreply.github.com>
2023-05-23 19:16:59 +0000

    conversion operation

--- 01_basics/03_conversionoperations.js
+++ 01_basics/03_conversionoperations.js
@@ -1,37 +1,55 @@
-let score = "true"
+// let score = "true"
 
-console.log(typeof score);
+// console.log(typeof score);
 
-console.log(typeof(score));
+// console.log(typeof(score));
 
-let  valueInNumber = Number(score);
+// let  valueInNumber = Number(score);
 
-console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber);
 
-console.log(valueInNumber);
+// console.log(valueInNumber);
 
-// "33" => 33
-// "33abc" => NaN (not a number)
-// "true" => 1 ; false => 0
+// // "33" => 33
+// // "33abc" => NaN (not a number)
+// // "true" => 1 ; false => 0
 
-let isLoggedIn = "hitesh"
+// let isLoggedIn = "hitesh"
 
-let booleanNumber = Boolean(isLoggedIn);
+// let booleanNumber = Boolean(isLoggedIn);
 
-console.log(booleanNumber);
+// console.log(booleanNumber);
 
-// 1 => true
-// 0 => false
-// "" => false 
-// "hitesh" => true
+// // 1 => true
+// // 0 => false
+// // "" => false 
+// // "hitesh" => true
 
-let india = 1233
+// let india = 1233
 
-let stringNumber = String(india)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let stringNumber = String(india)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
 // in the conversion method we can assign any variable name.
 // the formula is 
 // let any name of variable = datatype(variable which we wnt to change);
 // console.log(any name of the variable);
+
+
+// ***************************************opertions****************************************************
+
+// console.log(1+2+"3"+4+4+6+5+55);
+let str1 = 2
+let str2 = 3
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log(1+3)
+// console.log("1"+"3")
+// console.log(1+3+"2")
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
