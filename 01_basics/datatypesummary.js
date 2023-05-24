9a8a0fb539125ba9d7e0b07e0e54cd829b0e9a23
Sahil-athlete <134266260+Sahil-athlete@users.noreply.github.com>
2023-05-24 10:26:48 +0000

    datatypes summary

---
+++ 01_basics/datatypesummary.js
@@ -0,0 +1,54 @@
+/* there is two types of datatype
+1)primitive datatype 
+2)regular or nonprimetive datatype
+
+primitive datatype
+string, number, bollean, null, undefined, bigint, symbol 
+
+regular or nonprimitive 
+array, object, function */
+
+//premitive example
+
+const string = "123"
+
+const score = 100
+const scoreValue= 100.2
+
+const isLoggedIn = false
+
+const outSideTemp = null
+
+let userMail;
+
+const Id = Symbol ('2321')
+const uniqueId = Symbol ('2321')
+console.log(Id === uniqueId);
+
+const bigInt = 12333445321345522n
+
+//non premetive example
+
+const jangju = ["khalidbinwalid, umarfarooque, aliabnabitalib"]
+
+let obj= {
+
+    name:"sahil",
+    age: 21,
+}
+
+const myfunction =function(){
+    console.log("hellow world");
+}
+
+ 
+//console.log (typeof  jangju);
+
+
+
+ 
+
+
+
+
+
