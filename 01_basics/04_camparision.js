2420b8401a3c8172df87a6dc915c35ddc572de8c
Sahil-athlete <134266260+Sahil-athlete@users.noreply.github.com>
2023-05-24 07:13:56 +0000

    comparision

---
+++ 01_basics/04_camparision.js
@@ -0,0 +1,23 @@
+console.log( 1 > 2);
+console.log( 1 < 2);
+console.log( 1 == 2);
+console.log( 1 >= 2);
+console.log( 1 != 2);
+
+console.log( "2" > 3);
+console.log( "3" > 2);
+
+console.log( null > 0);
+console.log( null < 0);
+console.log( null >= 0);
+console.log( null <= 0);
+console.log( null >= 45);
+
+console.log( undefined > 0);
+console.log( undefined < 0);
+console.log( undefined <= 0);
+
+console.log( "2" === 2);
+
+/*this type of conversion can damanage the readiability of code 
+so dont prefer this type of conversion */
