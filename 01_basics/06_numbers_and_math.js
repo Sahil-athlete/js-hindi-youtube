fc1cdb0155cb585fd7e0c1083573f0c0d510d0e1
Sahil-athlete <134266260+Sahil-athlete@users.noreply.github.com>
2023-05-25 16:01:29 +0000

    numbers and math

--- 01_basics/06_numbers_and_math.js
+++ 01_basics/06_numbers_and_math.js
@@ -1,28 +1,37 @@
 const number = 300;
-// console.log(number);
+console.log(number);
 
 const anotherNumber =  new Number('100');
-// console.log(anotherNumber);
+console.log(anotherNumber);
 
-// console.log(anotherNumber.toString());
-// console.log(anotherNumber.toString().length);
+console.log(anotherNumber.toString());
+console.log(anotherNumber.toString().length);
  
-// console.log(anotherNumber.toFixed(2));
+console.log(anotherNumber.toFixed(2));
 
 const big = 100.3227
-// console.log (big.toPrecision(6));
+console.log (big.toPrecision(6));
 
 const bigNumber = 100000000;
-// console.log(bigNumber.toLocaleString('en-IN'));
+console.log(bigNumber.toLocaleString('en-IN'));
 
 // +++++++++++++++++++++++++++++++++MATHS++++++++++++++++++++++++++++++++++
 
-// console.log(Math);
-// console.log(Math.abs('2.3'));
-// console.log(Math.round('2.3')); 
-// console.log(Math.ceil('2.3'));
-// console.log(Math.floor('3.4'));
-// console.log(Math.min('2.3', 4, 3 ,5, 5));
-// console.log(Math.max('2.3', 4, 3 ,5, 5));
+console.log(Math);
+console.log(Math.abs('2.3'));
+console.log(Math.round('2.3')); 
+console.log(Math.ceil('2.3'));
+console.log(Math.floor('3.4'));
+console.log(Math.min('2.3', 4, 3 ,5, 5));
+console.log(Math.max('2.3', 4, 3 ,5, 5));
 
-console.log(Math.random('2.3', 4, 3 ,5, 5));
+console.log(Math.random());
+
+console.log(Math.floor(Math.random()* 10)+ 1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
+
+//console.log(Math.floor(Math.random() * (max - min + 1) + min)); this is the formula for random value with condition
