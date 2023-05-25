ea704de5abaec18e572609b71c0f6abe8e5b5f1e
Sahil-athlete <134266260+Sahil-athlete@users.noreply.github.com>
2023-05-25 07:51:30 +0000

    strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,10 +1,35 @@
-const myName = 'sahil'
-const runs = '1223'
+const myName = "sahil"
+const runs = 1223
 
 console.log(myName + runs  + "value");
+
 console.log(`my name is ${myName} and my runs in first innings = ${runs}`);
 
-const string= new String('sahil')
+const string= new String("freess-cftu-sahiul")
  
 console.log(string[3]);
+
 console.log(string.__proto__);
+
+console.log(string.length);
+console.log(string.toUpperCase());
+console.log(string.charAt(1));
+console.log(string.indexOf('l'));
+
+const newString = string.substring(0,4)
+console.log(newString);
+
+const anotherString = string.slice(-18, 4)
+console.log(anotherString);
+
+const newStringOne = "          sahil      "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://sahilkhan.com/khan123%30"
+
+console.log(url.replace('sahil', '20'))
+
+console.log(url.includes('sahil'));
+
+console.log(string.split('-'));
