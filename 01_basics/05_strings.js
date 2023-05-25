698fcab7a6c875802f5cc51b39ad66112620363e
Sahil-athlete <134266260+Sahil-athlete@users.noreply.github.com>
2023-05-25 10:35:20 +0000

    at using

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -6,7 +6,7 @@ console.log(myName + runs  + "value");
 console.log(`my name is ${myName} and my runs in first innings = ${runs}`);
 
 const string= new String("freess-cftu-sahiul")
- 
+
 console.log(string[3]);
 
 console.log(string.__proto__);
@@ -28,8 +28,16 @@ console.log(newStringOne.trim());
 
 const url = "https://sahilkhan.com/khan123%30"
 
-console.log(url.replace('sahil', '20'))
+console.log(url.replace('sahil', '.com'));
 
-console.log(url.includes('sahil'));
+console.log(url.includes('FALSE'));
 
-console.log(string.split('-'));
+const checkArray = 'i-am-a-boy'
+const newArray =checkArray.split('-')
+console.log(newArray);
+
+const array = [ 2, 12, 32, 42];
+let sock = 1;
+console.log(`the value of ${sock} is the value of ${array.at(sock)}`);
+sock = -2;
+console.log(`the value of ${sock} is the value of ${array.at(sock)}`);
