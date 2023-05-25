e03b7828e6ad82a328a308c28487f91b9aed9caf
Sahil-athlete <134266260+Sahil-athlete@users.noreply.github.com>
2023-05-25 13:39:18 +0000

    numbers

---
+++ 01_basics/06_numbers_and_math.js
@@ -0,0 +1,28 @@
+const number = 300;
+// console.log(number);
+
+const anotherNumber =  new Number('100');
+// console.log(anotherNumber);
+
+// console.log(anotherNumber.toString());
+// console.log(anotherNumber.toString().length);
+ 
+// console.log(anotherNumber.toFixed(2));
+
+const big = 100.3227
+// console.log (big.toPrecision(6));
+
+const bigNumber = 100000000;
+// console.log(bigNumber.toLocaleString('en-IN'));
+
+// +++++++++++++++++++++++++++++++++MATHS++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs('2.3'));
+// console.log(Math.round('2.3')); 
+// console.log(Math.ceil('2.3'));
+// console.log(Math.floor('3.4'));
+// console.log(Math.min('2.3', 4, 3 ,5, 5));
+// console.log(Math.max('2.3', 4, 3 ,5, 5));
+
+console.log(Math.random('2.3', 4, 3 ,5, 5));
