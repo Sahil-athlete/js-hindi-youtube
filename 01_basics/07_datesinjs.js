9438af51ea0741dd6d2c879b2b33b8c100c14a20
Sahil-athlete <134266260+Sahil-athlete@users.noreply.github.com>
2023-05-26 07:23:52 +0000

    dates

---
+++ 01_basics/07_datesinjs.js
@@ -0,0 +1,19 @@
+//Dates
+
+let currentDate = new Date()
+// console.log (currentDate.toDateString());
+// console.log (currentDate.toISOString());
+// console.log (currentDate.toJSON());
+// console.log (currentDate.toLocaleDateString());
+// console.log (currentDate.toLocaleTimeString());
+// console.log (currentDate.toTimeString());
+// console.log (currentDate.toUTCString());
+// console.log (currentDate.getTimezoneOffset());
+// console.log(typeof currentDate);
+
+
+// let createDate = new Date(2023, 00, 23)
+// let createDate = new Date(2023, 00, 23, 5, 23, 21)
+//let createDate = new Date("2023-01-21")
+//let createDate = new Date("01-05-2023")
+console.log(createDate.toLocaleString());
