0fda87427259ad66592d0e59b2f91e28cb8a8020
Sahil-athlete <134266260+Sahil-athlete@users.noreply.github.com>
2023-06-01 09:57:16 +0000

    array1

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,37 +1,37 @@
-// //Array
+//arrays 
+const arrays = [0,1,2,3,4,5,6]
+const superHeroes = ["spdiderman", "batman", "superman"]
+const array = new String(0,1,2,3,4,5,) 
+console.log(arrays[0]);
+
+//METHODS
 
 
- const arry =[1, 2, 3, 4, 5, 'sahil']
-console.log(arry[3]);
+arrays.push(7)
+arrays.unshift(8)
+arrays.shift()
+console.log(arrays);
 
-const arrays = new Array('sahil', 'sameer', 'imran')
-console.log(arrays[2]);
+console.log(arrays.includes(3));
+console.log(arrays.indexOf(5));
 
-// // ++++++++++++++++++++++++++++++++Method+++++++++++++++
+const arrays3 = arrays.join()
+console.log(arrays);
+console.log(arrays3);
+console.log(typeof arrays3);
 
-arry.push(6)
-arry.push(8)
-arry.pop()
+// slice splice 
 
-arry.unshift(8)
-arry.shift()
+console.log("A " , arrays);
 
-console.log(arry.includes(7));
-console.log(arrays.indexOf(2));
+const myna1= arrays.slice(1,3)
+console.log(myna1);
 
-console.log(arry);
+console.log("B " , arrays); //slice method dosn't change in the original refrence.
 
-const value = arry.join()
-console.log(arry);
-console.log(typeof value);
+console.log("C " , arrays);
 
-// slice and splice
+const myna2 = arrays.splice(1,3)
+console.log(myna2);
 
-console.log("A", arry)
-const anotherArray = arry.slice('1', '3')
-console.log(anotherArray);
-
-console.log("b", arry)
-const arraysThree = arry.splice('1','3')
-console.log("c", arry)
-console.log(arraysThree);
+console.log("D " , arrays); //splice method change in the original refrence.
